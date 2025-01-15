6219ed35946ce62b3f73d70a2a8e91b7fb4846c8
thevarsasuthesan <tsuthesan1996@gmail.com>
2025-01-15 20:29:38 +0530

    upadate footer page

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,18 +2,14 @@
     <div class="container">
         <div class="row">
             <!-- .widget -->
-            <div class="col-lg-3 col-md-3 col-sm-6 col-xs-6 widget">
-                <img class="positioned wow slideInUp "
-                     src="{{ asset('../build/assets/img/resources/footer-crane.png') }}" alt="" width="380px"
-                     height="372px">
-            </div> <!-- /.widget -->
+            {{--            <div class="col-lg-3 col-md-3 col-sm-6 col-xs-6 widget">--}}
+            {{--                <img class="positioned wow slideInUp "--}}
+            {{--                     src="{{ asset('../build/assets/img/resources/footer-crane.png') }}" alt="" width="380px"--}}
+            {{--                     height="372px">--}}
+            {{--            </div> <!-- /.widget -->--}}
             <!-- .widget -->
-            <div class="col-lg-3 col-md-3 col-sm-6 col-xs-6 widget">
-                <h3>About Us</h3>
-                <p>BuildExpert,established in 2023, <br>is on its way to becoming one of Australia's leading platforms
-                    in the Construction industry.We specialize in residential and commercial estimating,drafting and 3D
-                    modeling services,delivering expert solutions tailored to builders and trades.</p>
-                <a href="#" class="read-more">Read More <i class="fa fa-angle-double-right"></i></a>
+            <div class="col-lg-4 col-md-4 col-sm-6 col-xs-6 widget">
+                <h3 style="font-size: 50px">Build Expert</h3>
                 <ul class="social">
                     <li><a href="https://www.facebook.com/BuildExpertAus" class="hvr-radial-out"><i
                                 class="fa fa-facebook"></i></a></li>
@@ -23,28 +19,29 @@
                                 class="fa fa-linkedin"></i></a></li>
                 </ul>
             </div> <!-- /.widget -->
-            <!-- .widget -->
-            <div class="col-lg-3 col-md-3 col-sm-6 col-xs-6 widget clearboth-tab clearboth-480">
-                <h3>popular Posts</h3>
-                <ul class="our-services">
-                    <li><a href="#">Building Construction</a></li>
-                    <li><a href="#">Hardwood Flooring</a></li>
-                    <li><a href="#">Home Renovation</a></li>
-                    <li><a href="#">Repairing of Roof</a></li>
-                    <li><a href="#">Commercial Construction</a></li>
-                </ul>
-            </div> <!-- /.widget -->
-            <!-- .widget -->
+
             <div class="col-lg-3 col-md-3 col-sm-6 col-xs-6 widget">
                 <h3>Get in Touch</h3>
                 <ul class="contact-info">
-                    <li><i class="fa fa-map-marker"></i> <span
-                            class="text">6 Russell street <br>Nunawading 3131 VIC</span></li>
                     <li><i class="fa fa-phone"></i> <span class="text">0405723838</span></li>
                     <li><i class="fa fa-envelope-o"></i> <span class="text">info@buildexpert.com.au</span></li>
-                    <li><i class="fa fa-globe"></i> <span class="text">https://www.buildexpert.com.au</span></li>
+                    <li><i class="fa fa-map-marker"></i> <span
+                            class="text">6 Russell street <br>Nunawading 3131 VIC</span></li>
+                    {{--                    <li><i class="fa fa-globe"></i> <span class="text">https://www.buildexpert.com.au</span></li>--}}
+                </ul>
+            </div><!-- /.widget -->
+            <!-- .widget -->
+            <div class="col-lg-3 col-md-3 col-sm-6 col-xs-6 widget ">
+                <h3>Quick Links</h3>
+                <ul class="">
+                    <li><a href="{{ route('welcome') }}" style="color: #fff;text-transform: uppercase;font-weight: bold">Home </a></li>
+                    <br>
+                    <li><a href="" style="color: #fff;text-transform: uppercase;font-weight: bold">About Us</a></li>
+                    <br>
+                    <li><a href="#" style="color: #fff;text-transform: uppercase;font-weight: bold">Privacy Policy</a></li>
                 </ul>
             </div> <!-- /.widget -->
+            <!-- .widget -->
 
         </div>
     </div>
