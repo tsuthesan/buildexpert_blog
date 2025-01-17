e0ac901f3093cec96c00660a352900e52bea731e
thevarsasuthesan <tsuthesan1996@gmail.com>
2025-01-17 21:30:48 +0530

    update carousel

--- resources/views/layouts/carousel.blade.php
+++ resources/views/layouts/carousel.blade.php
@@ -6,14 +6,16 @@
                      data-bgposition="center top" data-bgfit="cover" data-bgrepeat="no-repeat">
 
                 <div class="tp-caption sfb tp-resizeme construct-banner-caption"
-                     data-x="right" data-hoffset="0"
-                     data-y="top" data-voffset="205"
+                     data-x="left" data-hoffset="0"
+                     data-y="top" data-voffset="250"
                      data-whitespace="nowrap"
                      data-transform_idle="o:1;"
                      data-transform_in="o:0"
                      data-transform_out="o:0"
                      data-start="500">
-                    {{--                    <h2><b>We Are Available For</b></h2>--}}
+                                        <h2><b>ESTIMATING</b></h2>
+                                        <h2><b>DRAFTING</b></h2>
+                                        <h2><b>3D RENDERING</b></h2>
                 </div>
                 <div class="tp-caption sfb tp-resizeme construct-banner-caption"
                      data-x="right" data-hoffset="20"
@@ -50,14 +52,14 @@
                     {{--                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Conse <br> quatur  earum iusto tempora nesciunt enim harum architecto, <br> quis culpa ducimus deleniti, labore. Libero magnam</p>--}}
                 </div>
                 <div class="tp-caption sfb tp-resizeme construct-banner-caption"
-                     data-x="right" data-hoffset="315"
+                     data-x="left" data-hoffset="50"
                      data-y="top" data-voffset="480"
                      data-whitespace="nowrap"
                      data-transform_idle="o:1;"
                      data-transform_in="o:0"
                      data-transform_out="o:0"
                      data-start="1700">
-                    {{--                    <a href="#" class="hvr-bounce-to-right">Learn More</a>--}}
+                                        <a href="#" class="hvr-bounce-to-right">Learn More</a>
                 </div>
             </li>
             <li data-transition="fade">
@@ -65,14 +67,16 @@
                      data-bgfit="cover" data-bgrepeat="no-repeat">
 
                 <div class="tp-caption sfb tp-resizeme construct-banner-caption"
-                     data-x="left" data-hoffset="270"
-                     data-y="top" data-voffset="205"
+                     data-x="left" data-hoffset="0"
+                     data-y="top" data-voffset="250"
                      data-whitespace="nowrap"
                      data-transform_idle="o:1;"
                      data-transform_in="o:0"
                      data-transform_out="o:0"
                      data-start="500">
-                    {{--                    <h2>We Renovate</h2>--}}
+                    <h2><b>ESTIMATING</b></h2>
+                    <h2><b>DRAFTING</b></h2>
+                    <h2><b>3D RENDERING</b></h2>
                 </div>
                 <div class="tp-caption sfb tp-resizeme construct-banner-caption"
                      data-x="left" data-hoffset="280"
@@ -120,14 +124,16 @@
                      data-bgfit="cover" data-bgrepeat="no-repeat">
 
                 <div class="tp-caption sfb tp-resizeme construct-banner-caption"
-                     data-x="center" data-hoffset="0"
-                     data-y="top" data-voffset="205"
+                     data-x="left" data-hoffset="0"
+                     data-y="top" data-voffset="250"
                      data-whitespace="nowrap"
                      data-transform_idle="o:1;"
                      data-transform_in="o:0"
                      data-transform_out="o:0"
                      data-start="700">
-                    {{--                    <h2>We Are The Leader</h2>--}}
+                    <h2><b>ESTIMATING</b></h2>
+                    <h2><b>DRAFTING</b></h2>
+                    <h2><b>3D RENDERING</b></h2>
                 </div>
                 <div class="tp-caption sfb tp-resizeme construct-banner-caption"
                      data-x="center" data-hoffset="0"
