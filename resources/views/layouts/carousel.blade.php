b8fe6664f70c980993c7bf1f0d346316813b82aa
thevarsasuthesan <tsuthesan1996@gmail.com>
2025-01-19 11:40:55 +0530

    update carousel

--- resources/views/layouts/carousel.blade.php
+++ resources/views/layouts/carousel.blade.php
@@ -7,174 +7,204 @@
 
                 <div class="tp-caption sfb tp-resizeme construct-banner-caption"
                      data-x="left" data-hoffset="0"
-                     data-y="top" data-voffset="250"
+                     data-y="top" data-voffset="200"
                      data-whitespace="nowrap"
                      data-transform_idle="o:1;"
                      data-transform_in="o:0"
                      data-transform_out="o:0"
                      data-start="500">
                                         <h2><b>ESTIMATING</b></h2>
-                                        <h2><b>DRAFTING</b></h2>
-                                        <h2><b>3D RENDERING</b></h2>
+
+{{--                                        <h2><b>3D RENDERING</b></h2>--}}
                 </div>
+{{--                <div class="tp-caption sfb tp-resizeme construct-banner-caption"--}}
+{{--                     data-x="right" data-hoffset="20"--}}
+{{--                     data-y="top" data-voffset="265"--}}
+{{--                     data-whitespace="nowrap"--}}
+{{--                     data-transform_idle="o:1;"--}}
+{{--                     data-transform_in="o:0"--}}
+{{--                     data-transform_out="o:0"--}}
+{{--                     data-start="800">--}}
+{{--                    --}}{{--                    <h2>Any Construction Work</h2>--}}
+{{--                </div>--}}
+{{--                <div class="tp-caption sfb tp-resizeme construct-banner-caption"--}}
+{{--                     data-x="right" data-hoffset="15"--}}
+{{--                     data-y="top" data-voffset="330"--}}
+{{--                     data-whitespace="nowrap"--}}
+{{--                     data-transform_idle="o:1;"--}}
+{{--                     data-transform_in="o:0"--}}
+{{--                     data-transform_out="o:0"--}}
+{{--                     data-start="1100">--}}
+{{--                    --}}{{--                    <ul>--}}
+{{--                    --}}{{--                        <li><span>Domestic</span></li>--}}
+{{--                    --}}{{--                        <li><span>Commercial</span></li>--}}
+{{--                    --}}{{--                        <li><span>Industrial</span></li>--}}
+{{--                    --}}{{--                    </ul>--}}
+{{--                </div>--}}
+{{--                <div class="tp-caption sfb tp-resizeme construct-banner-caption"--}}
+{{--                     data-x="right" data-hoffset="0"--}}
+{{--                     data-y="top" data-voffset="250"--}}
+{{--                     data-whitespace="nowrap"--}}
+{{--                     data-transform_idle="o:1;"--}}
+{{--                     data-transform_in="o:0"--}}
+{{--                     data-transform_out="o:0"--}}
+{{--                     data-start="1400">--}}
+{{--                    --}}{{--                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Conse <br> quatur  earum iusto tempora nesciunt enim harum architecto, <br> quis culpa ducimus deleniti, labore. Libero magnam</p>--}}
+{{--                </div>--}}
                 <div class="tp-caption sfb tp-resizeme construct-banner-caption"
-                     data-x="right" data-hoffset="20"
-                     data-y="top" data-voffset="265"
+                     data-x="left" data-hoffset="0"
+                     data-y="top" data-voffset="280"
                      data-whitespace="nowrap"
                      data-transform_idle="o:1;"
                      data-transform_in="o:0"
                      data-transform_out="o:0"
-                     data-start="800">
-                    {{--                    <h2>Any Construction Work</h2>--}}
+                     data-start="600">
+{{--                                        <a href="#" class="hvr-bounce-to-right">Learn More</a>--}}
+                    <h2><b>DRAFTING</b></h2>
                 </div>
                 <div class="tp-caption sfb tp-resizeme construct-banner-caption"
-                     data-x="right" data-hoffset="15"
-                     data-y="top" data-voffset="330"
+                     data-x="left" data-hoffset="0"
+                     data-y="top" data-voffset="360"
                      data-whitespace="nowrap"
                      data-transform_idle="o:1;"
                      data-transform_in="o:0"
                      data-transform_out="o:0"
-                     data-start="1100">
-                    {{--                    <ul>--}}
-                    {{--                        <li><span>Domestic</span></li>--}}
-                    {{--                        <li><span>Commercial</span></li>--}}
-                    {{--                        <li><span>Industrial</span></li>--}}
-                    {{--                    </ul>--}}
-                </div>
-                <div class="tp-caption sfb tp-resizeme construct-banner-caption"
-                     data-x="right" data-hoffset="0"
-                     data-y="top" data-voffset="350"
-                     data-whitespace="nowrap"
-                     data-transform_idle="o:1;"
-                     data-transform_in="o:0"
-                     data-transform_out="o:0"
-                     data-start="1400">
-                    {{--                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Conse <br> quatur  earum iusto tempora nesciunt enim harum architecto, <br> quis culpa ducimus deleniti, labore. Libero magnam</p>--}}
-                </div>
-                <div class="tp-caption sfb tp-resizeme construct-banner-caption"
-                     data-x="left" data-hoffset="50"
-                     data-y="top" data-voffset="480"
-                     data-whitespace="nowrap"
-                     data-transform_idle="o:1;"
-                     data-transform_in="o:0"
-                     data-transform_out="o:0"
-                     data-start="1700">
-                                        <a href="#" class="hvr-bounce-to-right">Learn More</a>
+                     data-start="700">
+                    {{--                                        <a href="#" class="hvr-bounce-to-right">Learn More</a>--}}
+                    <h2><b>3D RENDERING</b></h2>
                 </div>
             </li>
             <li data-transition="fade">
                 <img src=" {{ asset('/build/assets/img/slides/image02.jpg') }}" alt="" data-bgposition="center top"
                      data-bgfit="cover" data-bgrepeat="no-repeat">
 
+
                 <div class="tp-caption sfb tp-resizeme construct-banner-caption"
                      data-x="left" data-hoffset="0"
-                     data-y="top" data-voffset="250"
+                     data-y="top" data-voffset="200"
                      data-whitespace="nowrap"
                      data-transform_idle="o:1;"
                      data-transform_in="o:0"
                      data-transform_out="o:0"
                      data-start="500">
                     <h2><b>ESTIMATING</b></h2>
-                    <h2><b>DRAFTING</b></h2>
-                    <h2><b>3D RENDERING</b></h2>
-                </div>
-                <div class="tp-caption sfb tp-resizeme construct-banner-caption"
-                     data-x="left" data-hoffset="280"
-                     data-y="top" data-voffset="255"
-                     data-whitespace="nowrap"
-                     data-transform_idle="o:1;"
-                     data-transform_in="o:0"
-                     data-transform_out="o:0"
-                     data-start="1000">
-                    {{--                    <h2>Your home with heart</h2>--}}
-                </div>
-                <div class="tp-caption sfl tp-resizeme construct-banner-caption"
-                     data-x="left" data-hoffset="290"
-                     data-y="top" data-voffset="320"
-                     data-whitespace="nowrap"
-                     data-transform_idle="o:1;"
-                     data-transform_in="o:0"
-                     data-transform_out="o:0"
-                     data-start="1500">
-                    {{--                    <span class="border"></span>--}}
-                </div>
-                <div class="tp-caption sfb tp-resizeme construct-banner-caption"
-                     data-x="left" data-hoffset="290"
-                     data-y="top" data-voffset="315"
-                     data-whitespace="nowrap"
-                     data-transform_idle="o:1;"
-                     data-transform_in="o:0"
-                     data-transform_out="o:0"
-                     data-start="2000">
-                    {{--                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed <br> do eiusmod tempor incididunt ut labore et dolore.</p>--}}
-                </div>
-                <div class="tp-caption sfb tp-resizeme construct-banner-caption"
-                     data-x="left" data-hoffset="290"
-                     data-y="top" data-voffset="410"
-                     data-whitespace="nowrap"
-                     data-transform_idle="o:1;"
-                     data-transform_in="o:0"
-                     data-transform_out="o:0"
-                     data-start="2500">
-                    {{--                    <a href="#" class="hvr-bounce-to-right">Learn More</a>--}}
-                </div>
-            </li>
-            <li data-transition="fade">
-                <img src=" {{ asset('/build/assets/img/slides/image03.jpg') }}" alt="" data-bgposition="center top"
-                     data-bgfit="cover" data-bgrepeat="no-repeat">
 
+                    {{--                                        <h2><b>3D RENDERING</b></h2>--}}
+                </div>
                 <div class="tp-caption sfb tp-resizeme construct-banner-caption"
                      data-x="left" data-hoffset="0"
-                     data-y="top" data-voffset="250"
+                     data-y="top" data-voffset="280"
+                     data-whitespace="nowrap"
+                     data-transform_idle="o:1;"
+                     data-transform_in="o:0"
+                     data-transform_out="o:0"
+                     data-start="600">
+                    {{--                                        <a href="#" class="hvr-bounce-to-right">Learn More</a>--}}
+                    <h2><b>DRAFTING</b></h2>
+                </div>
+                <div class="tp-caption sfb tp-resizeme construct-banner-caption"
+                     data-x="left" data-hoffset="0"
+                     data-y="top" data-voffset="360"
                      data-whitespace="nowrap"
                      data-transform_idle="o:1;"
                      data-transform_in="o:0"
                      data-transform_out="o:0"
                      data-start="700">
-                    <h2><b>ESTIMATING</b></h2>
-                    <h2><b>DRAFTING</b></h2>
+                    {{--                                        <a href="#" class="hvr-bounce-to-right">Learn More</a>--}}
                     <h2><b>3D RENDERING</b></h2>
                 </div>
+
+            </li>
+            <li data-transition="fade">
+                <img src=" {{ asset('/build/assets/img/slides/image03.jpg') }}" alt="" data-bgposition="center top"
+                     data-bgfit="cover" data-bgrepeat="no-repeat">
                 <div class="tp-caption sfb tp-resizeme construct-banner-caption"
-                     data-x="center" data-hoffset="0"
-                     data-y="top" data-voffset="255"
+                     data-x="left" data-hoffset="0"
+                     data-y="top" data-voffset="200"
                      data-whitespace="nowrap"
                      data-transform_idle="o:1;"
                      data-transform_in="o:0"
                      data-transform_out="o:0"
-                     data-start="1000">
-                    {{--                    <h2>In Construction Company</h2>--}}
+                     data-start="500">
+                    <h2><b>ESTIMATING</b></h2>
+
+                    {{--                                        <h2><b>3D RENDERING</b></h2>--}}
                 </div>
                 <div class="tp-caption sfb tp-resizeme construct-banner-caption"
-                     data-x="center" data-hoffset="0"
-                     data-y="top" data-voffset="320"
+                     data-x="left" data-hoffset="0"
+                     data-y="top" data-voffset="280"
                      data-whitespace="nowrap"
                      data-transform_idle="o:1;"
                      data-transform_in="o:0"
                      data-transform_out="o:0"
-                     data-start="1300">
-                    {{--                    <span class="border"></span>--}}
+                     data-start="600">
+                    {{--                                        <a href="#" class="hvr-bounce-to-right">Learn More</a>--}}
+                    <h2><b>DRAFTING</b></h2>
                 </div>
                 <div class="tp-caption sfb tp-resizeme construct-banner-caption"
-                     data-x="center" data-hoffset="0"
-                     data-y="top" data-voffset="315"
+                     data-x="left" data-hoffset="0"
+                     data-y="top" data-voffset="360"
                      data-whitespace="nowrap"
                      data-transform_idle="o:1;"
                      data-transform_in="o:0"
                      data-transform_out="o:0"
-                     data-start="1600">
-                    {{--                    <p class="text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed <br> do eiusmod tempor incididunt ut labore et dolore.</p>--}}
-                </div>
-                <div class="tp-caption sfb tp-resizeme construct-banner-caption"
-                     data-x="center" data-hoffset="0"
-                     data-y="top" data-voffset="410"
-                     data-whitespace="nowrap"
-                     data-transform_idle="o:1;"
-                     data-transform_in="o:0"
-                     data-transform_out="o:0"
-                     data-start="1900">
-                    {{--                    <a href="#" class="hvr-bounce-to-right">Learn More</a>--}}
-                </div>
+                     data-start="700">
+                    {{--                                        <a href="#" class="hvr-bounce-to-right">Learn More</a>--}}
+                    <h2><b>3D RENDERING</b></h2>
+                </div
+
+{{--                <div class="tp-caption sfb tp-resizeme construct-banner-caption"--}}
+{{--                     data-x="left" data-hoffset="0"--}}
+{{--                     data-y="top" data-voffset="250"--}}
+{{--                     data-whitespace="nowrap"--}}
+{{--                     data-transform_idle="o:1;"--}}
+{{--                     data-transform_in="o:0"--}}
+{{--                     data-transform_out="o:0"--}}
+{{--                     data-start="700">--}}
+{{--                    <h2><b>ESTIMATING</b></h2>--}}
+{{--                    <h2><b>DRAFTING</b></h2>--}}
+{{--                    <h2><b>3D RENDERING</b></h2>--}}
+{{--                </div>--}}
+{{--                <div class="tp-caption sfb tp-resizeme construct-banner-caption"--}}
+{{--                     data-x="center" data-hoffset="0"--}}
+{{--                     data-y="top" data-voffset="255"--}}
+{{--                     data-whitespace="nowrap"--}}
+{{--                     data-transform_idle="o:1;"--}}
+{{--                     data-transform_in="o:0"--}}
+{{--                     data-transform_out="o:0"--}}
+{{--                     data-start="1000">--}}
+{{--                    --}}{{--                    <h2>In Construction Company</h2>--}}
+{{--                </div>--}}
+{{--                <div class="tp-caption sfb tp-resizeme construct-banner-caption"--}}
+{{--                     data-x="center" data-hoffset="0"--}}
+{{--                     data-y="top" data-voffset="320"--}}
+{{--                     data-whitespace="nowrap"--}}
+{{--                     data-transform_idle="o:1;"--}}
+{{--                     data-transform_in="o:0"--}}
+{{--                     data-transform_out="o:0"--}}
+{{--                     data-start="1300">--}}
+{{--                    --}}{{--                    <span class="border"></span>--}}
+{{--                </div>--}}
+{{--                <div class="tp-caption sfb tp-resizeme construct-banner-caption"--}}
+{{--                     data-x="center" data-hoffset="0"--}}
+{{--                     data-y="top" data-voffset="315"--}}
+{{--                     data-whitespace="nowrap"--}}
+{{--                     data-transform_idle="o:1;"--}}
+{{--                     data-transform_in="o:0"--}}
+{{--                     data-transform_out="o:0"--}}
+{{--                     data-start="1600">--}}
+{{--                    --}}{{--                    <p class="text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed <br> do eiusmod tempor incididunt ut labore et dolore.</p>--}}
+{{--                </div>--}}
+{{--                <div class="tp-caption sfb tp-resizeme construct-banner-caption"--}}
+{{--                     data-x="center" data-hoffset="0"--}}
+{{--                     data-y="top" data-voffset="410"--}}
+{{--                     data-whitespace="nowrap"--}}
+{{--                     data-transform_idle="o:1;"--}}
+{{--                     data-transform_in="o:0"--}}
+{{--                     data-transform_out="o:0"--}}
+{{--                     data-start="1900">--}}
+{{--                    --}}{{--                    <a href="#" class="hvr-bounce-to-right">Learn More</a>--}}
+{{--                </div>--}}
             </li>
         </ul>
     </div>
